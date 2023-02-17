a760773bbc4c6750a3975c99b73d336bc3d20f53
jacky <qujiakang@gmail.com>
2023-02-18 00:28:46 +0800

    init

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,40 +1,46 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <flat-pickr v-model="date"/>
+    <loading :active.sync="isLoading"
+                 :can-cancel="true"
+                 :on-cancel="onCancel"
+                 :is-full-page="fullPage"></loading>
+    <b-button variant="danger" @click.prevent="load">Button</b-button>
+    <el-button type="primary">主要按钮</el-button>
   </div>
 </template>
 
 <script>
+import flatPickr from 'vue-flatpickr-component';
+import 'flatpickr/dist/flatpickr.css';
+import Loading from 'vue-loading-overlay';
+import 'vue-loading-overlay/dist/vue-loading.css';
 export default {
   name: 'HelloWorld',
+  components:{
+    flatPickr,Loading
+  },
   props: {
     msg: String
+  },
+  data(){
+    return {
+      isLoading: false,
+      fullPage: true,
+      date:null
+    }
+  },
+  methods:{
+    load:function(){
+      this.isLoading = true;
+          // simulate AJAX
+           setTimeout(() => {
+                this.isLoading = false
+           }, 5000)
+    },
+    onCancel() {
+        console.log('User cancelled the loader.')
+    }
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,19 @@
 import Vue from 'vue'
 import App from './App.vue'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+import { BootstrapVue } from 'bootstrap-vue'
+import { useAntd } from 'k-form-design/packages/core/useComponents'
+import KFormDesign from 'k-form-design/packages/use.js'
+import 'k-form-design/lib/k-form-design.css'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+useAntd(Vue)
+Vue.use(KFormDesign)
+Vue.use(BootstrapVue)
+Vue.use(ElementUI)
 
+import 'ant-design-vue/dist/antd.css'
 Vue.config.productionTip = false
 
 new Vue({

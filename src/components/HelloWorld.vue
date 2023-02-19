211b7ed5428f2c1a7065ac38ecb4dd20df1ed42d
jacky <qujiakang@gmail.com>
2023-02-19 18:18:14 +0800

    add more libs

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -6,7 +6,25 @@
                  :on-cancel="onCancel"
                  :is-full-page="fullPage"></loading>
     <b-button variant="danger" @click.prevent="load">Button</b-button>
-    <el-button type="primary">主要按钮</el-button>
+    <el-button type="primary" @click="testPina">主要按钮</el-button>
+    <file-upload
+    ref="upload"
+    v-model="files"
+    post-action="/post.method"
+    put-action="/put.method"
+    @input-file="inputFile"
+    @input-filter="inputFilter"
+  >
+  Upload file
+  </file-upload>
+  <ul>
+    <li v-for="file in files" :key="file.name">{{file.name}} - Error: {{file.error}}, Success: {{file.success}}</li>
+  </ul>
+  <ul>
+    <li v-for="t in tagslist" :key="t.key">
+       {{ t.title }}
+    </li>
+  </ul>
   </div>
 </template>
 
@@ -15,22 +33,31 @@ import flatPickr from 'vue-flatpickr-component';
 import 'flatpickr/dist/flatpickr.css';
 import Loading from 'vue-loading-overlay';
 import 'vue-loading-overlay/dist/vue-loading.css';
+import VueUploadComponent from 'vue-upload-component';
+import { mapState,mapActions } from 'pinia' //引入映射函数
+import useStore from '../store/index' //引入store
 export default {
   name: 'HelloWorld',
   components:{
-    flatPickr,Loading
+    flatPickr,Loading,
+    FileUpload: VueUploadComponent
   },
   props: {
     msg: String
   },
+  computed:{
+    ...mapState(useStore,["tagslist"]) //映射函数，取出tagslist
+  },
   data(){
     return {
+      files:[],
       isLoading: false,
       fullPage: true,
       date:null
     }
   },
   methods:{
+    ...mapActions(useStore, ['changeTagList']), //映射action
     load:function(){
       this.isLoading = true;
           // simulate AJAX
@@ -40,6 +67,52 @@ export default {
     },
     onCancel() {
         console.log('User cancelled the loader.')
+    },
+    testPina() {
+           const t = {
+                title: 'OA',
+                key: 'oa',
+                closable: true
+            };
+            this.changeTagList(t);
+    },
+    /**
+     * Has changed
+     * @param  Object|undefined   newFile   Read only
+     * @param  Object|undefined   oldFile   Read only
+     * @return undefined
+     */
+    inputFile: function (newFile, oldFile) {
+      if (newFile && oldFile && !newFile.active && oldFile.active) {
+        // Get response data
+        console.log('response', newFile.response)
+        if (newFile.xhr) {
+          //  Get the response status code
+          console.log('status', newFile.xhr.status)
+        }
+      }
+    },
+    /**
+     * Pretreatment
+     * @param  Object|undefined   newFile   Read and write
+     * @param  Object|undefined   oldFile   Read only
+     * @param  Function           prevent   Prevent changing
+     * @return undefined
+     */
+    inputFilter: function (newFile, oldFile, prevent) {
+      if (newFile && !oldFile) {
+        // Filter non-image file
+        if (!/\.(jpeg|jpe|jpg|gif|png|webp)$/i.test(newFile.name)) {
+          return prevent()
+        }
+      }
+
+      // Create a blob field
+      newFile.blob = ''
+      let URL = window.URL || window.webkitURL
+      if (URL && URL.createObjectURL) {
+        newFile.blob = URL.createObjectURL(newFile.file)
+      }
     }
   }
 }
--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
 import App from './App.vue'
+import { createPinia, PiniaVuePlugin } from 'pinia'
+import { createPersistedState } from 'pinia-persistedstate-plugin'
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 import { BootstrapVue } from 'bootstrap-vue'
@@ -8,14 +11,17 @@ import KFormDesign from 'k-form-design/packages/use.js'
 import 'k-form-design/lib/k-form-design.css'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
-useAntd(Vue)
+Vue.use(VueRouter)
+Vue.use(PiniaVuePlugin)
 Vue.use(KFormDesign)
 Vue.use(BootstrapVue)
 Vue.use(ElementUI)
-
+useAntd(Vue)
 import 'ant-design-vue/dist/antd.css'
 Vue.config.productionTip = false
-
+const pinia = createPinia()
+pinia.use(createPersistedState()) //开启缓存，存储在localstorage
 new Vue({
-  render: h => h(App),
+  pinia,
+  render: h => h(App),  
 }).$mount('#app')
